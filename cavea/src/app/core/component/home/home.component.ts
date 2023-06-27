f1324c37b8e504b9aa194c6fd79700bf99083b38
Vazha Mikadze <74679350+mikadze13@users.noreply.github.com>
2023-06-27 22:51:58 +0400

    update baseUrl link

--- cavea/src/app/core/component/home/home.component.ts
+++ cavea/src/app/core/component/home/home.component.ts
@@ -43,7 +43,7 @@ export class HomeComponent implements OnInit {
 
     setTimeout(() => {
       this.lengthOfArr = this.itemsArr.length
-    }, 100);
+    }, 400);
     this.itemService.itemsUpdated$.subscribe((items) => {
       this.arrr = items
       setTimeout(() => {
--- cavea/src/environments/environments.ts
+++ cavea/src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production:false,
-    baseUrl:'https://caveacinema-5e8d9-default-rtdb.europe-west1.firebasedatabase.app/'
+    baseUrl:'https://caveacinema-95bf8-default-rtdb.europe-west1.firebasedatabase.app/'
 }
